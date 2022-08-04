48b4f78dcec619cba33779c5e29ceeae09abe596
drusya796 <107772373+drusya796@users.noreply.github.com>
2022-08-04 18:22:35 +0300

    homework is done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict"
+// let userName = prompt('What is your name?', "Sergey");
+// alert(`Your name is ${userName}!`)
+
+// let x = 12;
+// let y = 4;
+
+// let sum = x + y;
+// console.log(sum)
+// alert(sum);
+
+// let sub = x - y;
+// console.log(sub);
+// alert(sub);
+
+// let mult = x * y;
+// console.log(mult);
+// alert(mult);
+
+// let div = x / y;
+// console.log(div);
+// alert(div);
+
+let exp = (x+y) ** 3;
+console.log(exp);
+alert(exp);
